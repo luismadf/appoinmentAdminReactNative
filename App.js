002e5de3a514ada11ba73e3774007b390af4fe10
GRANJAW7 <granjaw7@macbook-pro-de-granjaw7.home>
2021-10-22 20:09:38 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+
+
+import React, {useState} from 'react';
+import {
+  Text, StyleSheet, View, FlatList
+} from 'react-native';
+
+const App = () => {
+
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'no come'},
+    {id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'no come'},
+    {id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'no come'},
+  ])
+
+  return (
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Administrador de Citas</Text>
+
+      <FlatList data={citas} renderItem={({item}) => {
+        return (
+          <View>
+            <Text>{item.paciente}</Text>
+          </View>
+        )
+      }} keyExtractor={cita => cita.id}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#AA076D',
+    flex: 1,
+  },
+  titulo: {
+    color: '#FFF',
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
+
+export default App;
