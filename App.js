453082f8d30c0ac32be607f9433424208e324885
GRANJAW7 <granjaw7@macbook-pro-de-granjaw7.home>
2021-10-23 19:15:28 +0200

    finishing multiple screen

--- App.js
+++ App.js
@@ -1,39 +1,124 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View, FlatList } from "react-native";
-import Cita from "./components/Cita";
+import React, { useState } from 'react'
+import {
+    Text,
+    StyleSheet,
+    View,
+    FlatList,
+    TouchableHighlight,
+    Platform
+} from 'react-native'
+import Cita from './components/Cita'
+import Formulario from './components/Formulario'
 
 const App = () => {
-  const [citas, setCitas] = useState([
-    { id: "1", paciente: "Hook", propietario: "Juan", sintomas: "no come" },
-    { id: "2", paciente: "Redux", propietario: "Itzel", sintomas: "no come" },
-    { id: "3", paciente: "Native", propietario: "Josue", sintomas: "no come" },
-  ]);
+    const [citas, setCitas] = useState([
+        { id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'no come' },
+        {
+            id: '2',
+            paciente: 'Redux',
+            propietario: 'Itzel',
+            sintomas: 'no come',
+        },
+        {
+            id: '3',
+            paciente: 'Native',
+            propietario: 'Josue',
+            sintomas: 'no come',
+        },
+    ])
 
-  return (
-    <View style={styles.contenedor}>
-      <Text style={styles.titulo}>Administrador de Citas</Text>
+    const [mostrarForm, setMostrarForm] = useState(false)
 
-      <FlatList
-        data={citas}
-        renderItem={({ item }) => <Cita cita={item} />}
-        keyExtractor={(cita) => cita.id}
-      />
-    </View>
-  );
-};
+    const eliminarPaciente = (id) => {
+        setCitas((citasActuales) => {
+            return citasActuales.filter((cita) => cita.id !== id)
+        })
+    }
+
+    const mostrarFormulario = () => {
+        setMostrarForm(!mostrarForm)
+    }
+
+    return (
+        <View style={styles.contenedor}>
+            <Text style={styles.titulo}>Administrador de Citas</Text>
+
+            <TouchableHighlight
+                onPress={() => mostrarFormulario()}
+                style={styles.btnMostrarForm}
+            >
+                <Text style={styles.textMostrarForm}>Crear Nueva Cita</Text>
+            </TouchableHighlight>
+
+            <View style={styles.contenido}>
+                {mostrarForm ? (<><Text style={styles.subTitulo}>
+                            Crear Nueva Cita
+                        </Text>
+                    <Formulario /></>
+                ) : (
+                    <>
+                        <Text style={styles.subTitulo}>
+                            {citas.length > 0
+                                ? 'Administra tus citas'
+                                : 'No hay citas'}
+                        </Text>
+
+                        <FlatList
+                            style={styles.listado}
+                            data={citas}
+                            renderItem={({ item }) => (
+                                <Cita
+                                    cita={item}
+                                    eliminarPaciente={eliminarPaciente}
+                                />
+                            )}
+                            keyExtractor={(cita) => cita.id}
+                        />
+                    </>
+                )}
+            </View>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  contenedor: {
-    backgroundColor: "#AA076D",
-    flex: 1,
-  },
-  titulo: {
-    color: "#FFF",
-    marginTop: 40,
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+    contenedor: {
+        backgroundColor: '#AA076D',
+        flex: 1,
+    },
+    contenido: {
+        flex: 1,
+        marginHorizontal: '2.5%',
+    },
+    listado: {
+        flex: 1,
+    },
+    titulo: {
+        color: '#FFF',
+        marginTop: Platform.OS === 'ios' ? 40 : 20,
+        marginBottom: 20,
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    subTitulo: {
+        color: '#FFF',
+        marginTop: 20,
+        marginBottom: 20,
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    btnMostrarForm: {
+        padding: 10,
+        backgroundColor: '#7D024E',
+        marginVertical: 10,
+    },
+    textMostrarForm: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+})
 
-export default App;
+export default App
